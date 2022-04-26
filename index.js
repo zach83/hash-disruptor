187cf5941843de9783928843b6a5d8fa549f4f68
zach83 <95778027+zach83@users.noreply.github.com>
2022-04-26 15:14:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const fs = require("fs");
 const process = require("process");
 // Maybe a hash module to show before and after?
 
-const fileName = process.argv[2];
-const file = fs.readFile(fileName);
+const targetFileName = process.argv[2];
+const targetFileType = targetFileName.split(".").pop();
+const targetFileContent = fs.readFile(fileName);
 
-const fileTypes = []
-// If there is a file that does not have a commenting syntax, but can have data inserted, have its index contain a note.
-// If there is a file that does not have a commenting syntax, and cannot have data inserted, have its index contain a note
-const fileTypeCommentStart = []
-const fileTypeCommentEnd = []
+// When adding file information, leave out the "." in the fileTypes array.
+const fileTypes = ["html"]
+// If there is a file that does not have a commenting syntax, but can have data inserted, have its index contain "bin".
+// If there is a file that does not have a commenting syntax, and cannot have data inserted, have its index contain null.
+const fileTypeCommentStart = ["<!--"]
+const fileTypeCommentEnd = ["-->"]
+
+if ()
+{
+  
+} else {
+
+}
