96b3230cf588761a7bdc684fe0f5cb1650e7080a
zach83 <95778027+zach83@users.noreply.github.com>
2022-04-27 02:42:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-// const chalk = require("chalk");
+const crypto = require("crypto");
 const fs = require("fs");
 const process = require("process");
-// Maybe a hash module to show before and after?
 
 const targetFileName = process.argv[2];
 const targetFileType = targetFileName.split(".").pop();
 const targetFileContent = fs.readFile(fileName);
+const targetFileHash = crypto.createHash("md5", targetFileContent);
 
 // When adding file information, leave out the "." in the fileTypes array.
+// If there is a file that does not have a commenting syntax, and cannot have data inserted, do not include it.
 const fileTypes = ["html"]
 // If there is a file that does not have a commenting syntax, but can have data inserted, have its index contain "bin".
-// If there is a file that does not have a commenting syntax, and cannot have data inserted, have its index contain null.
 const fileTypeCommentStart = ["<!--"]
 const fileTypeCommentEnd = ["-->"]
 
-if ()
-{
-  
-} else {
+const isFileTypeRecognizable = fileTypes.includes(targetFileType);
+const isFileCommentBinary = fileTypeCommentStart[fileTypes.indexOf(targetFileType)];
 
+// Disrupt file hash
+if (isFileTypeRecognizable && (isFileCommentBinary !== "bin")) {
+  
+} else if (isFileTypeRecognizable && (isFileCommentBinary === "bin")) {
+
+} else {
+  console.error("Could not recognize this type of file.")
+  return;
 }
