795cd4b691f8abe98a61ee98047efcb48d7bcf8e
zach83 <95778027+zach83@users.noreply.github.com>
2022-05-21 00:11:37 -0400

    begin argument processing

--- index.js
+++ index.js
@@ -2,10 +2,33 @@ const crypto = require("crypto");
 const fs = require("fs");
 const process = require("process");
 
+var amount = 0;
+var hashMethod = "md5";
+var targetDir = ".";
+
+if () {
+
+}
+
+if () {
+  
+}
+
+if (process.argv.includes("-h") || process.argv.includes("--help") || process.argv.length <= 2) {
+  console.log("----------");
+  console.log("hash-disruptor - Disrupt a file's hash signature by injecting random data!");
+  console.log("USAGE: `node index.js <target file name> <arguments (optional)>`");
+  console.log("-a, --amount: Makes a certain amount of unique copies/editions of the target file.");
+  console.log("-d, --dir: Specifies a target directory to put the copies in.");
+  console.log("-e, --encryption: Specifies the type of hash you would like to ");
+  console.log("-h, --help: Displays this prompt.");
+  console.log("----------");
+}
+
 const targetFileName = process.argv[2];
 const targetFileType = targetFileName.split(".").pop();
 const targetFileContent = fs.readFile(fileName);
-const targetFileHash = crypto.createHash("md5", targetFileContent);
+const targetFileOriginalHash = crypto.createHash(hashMethod, targetFileContent);
 
 // When adding file information, leave out the "." in the fileTypes array.
 // If there is a file that does not have a commenting syntax, and cannot have data inserted, do not include it.
